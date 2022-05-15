bf03c89e5ef38e33a9abfccb6e16986e39343c4f
Brandon O'Briant <obrianbl@protonmail.com>
2022-05-15 09:42:12 -0800

    learning files added

---
+++ learning_folder/learn_util.py
@@ -0,0 +1,56 @@
+"""
+Author: 
+Date: 2022.05.15
+Description: File to learn Python and Utility functions for CV/Flask
+Reference: 
+"""
+#############################################
+#         Import Statements        
+#############################################
+"""
+Import json: 
+Ref: https://docs.python.org/3/library/json.html
+Description/Notes:
+JSON is a data <<something something>>
+> What is JSON:
+    - https://www.json.org/json-en.html
+> Important Examples: 
+- https://realpython.com/python-json/
+- https://www.w3schools.com/python/python_json.asp
+"""
+import json
+
+
+"""
+Import OS:
+Ref:
+Description/Notes:
+ - https://docs.python.org/3/library/os.html
+"""
+import os
+
+#############################################
+#        Functions       
+#############################################
+
+def get_base_url(port):
+    """ Gets the base url
+
+    Description/Notes:
+    - info: 
+        > json.load: https://www.geeksforgeeks.org/json-loads-in-python/
+            - notes about it
+
+    Args:
+
+    Returns:
+    """
+    pass
+
+
+def allowed_file(filename, ALLOWED_EXTENSIONS=set(['png', 'jpg', 'jpeg'])):
+    pass
+
+# function to add correct and or comma
+def and_syntax(alist):
+    pass
